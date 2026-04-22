6359a8c5fa42936da42d522a66bcc61042a90227
spi22 <spi22@kmslab.com>
2026-04-22 13:24:58 +0900

    menu storybook 소스 수정

--- components/menu/menu.stories.js
+++ components/menu/menu.stories.js
@@ -12,20 +12,18 @@ export default {
 
 export const Default = {
   render: () => `
-    <div style="padding:16px;width:240px;">
-      <div class="klds-menu">
-        <div class="klds-menu__item">
-          <span class="klds-menu__item-text">안 읽은 메일함</span>
-        </div>
-        <div class="klds-menu__item is-hover">
-          <span class="klds-menu__item-text">받은 메일함</span>
-        </div>
-        <div class="klds-menu__item">
-          <span class="klds-menu__item-text">보낸 메일함</span>
-        </div>
-        <div class="klds-menu__item">
-          <span class="klds-menu__item-text">임시 메일함</span>
-        </div>
+    <div class="klds-menu">
+      <div class="klds-menu__item">
+        <span class="klds-menu__item-text">안 읽은 메일함</span>
+      </div>
+      <div class="klds-menu__item is-hover">
+        <span class="klds-menu__item-text">받은 메일함</span>
+      </div>
+      <div class="klds-menu__item">
+        <span class="klds-menu__item-text">보낸 메일함</span>
+      </div>
+      <div class="klds-menu__item">
+        <span class="klds-menu__item-text">임시 메일함</span>
       </div>
     </div>
   `,
@@ -33,23 +31,21 @@ export const Default = {
 
 export const WithIcon = {
   render: () => `
-    <div style="padding:16px;width:240px;">
-      <div class="klds-menu">
-        <div class="klds-menu__item">
-          <div class="klds-menu__item-icon"
-            style="-webkit-mask-image:url(/assets/icons/ic-mail.svg);mask-image:url(/assets/icons/ic-mail.svg);background-color:var(--klds-color-text-black,#121212);"></div>
-          <span class="klds-menu__item-text">메일 편지함</span>
-        </div>
-        <div class="klds-menu__item is-hover">
-          <div class="klds-menu__item-icon"
-            style="-webkit-mask-image:url(/assets/icons/ic-folder.svg);mask-image:url(/assets/icons/ic-folder.svg);background-color:var(--klds-color-primary,#005386);"></div>
-          <span class="klds-menu__item-text">공용 폴더 (Hover)</span>
-        </div>
-        <div class="klds-menu__item">
-          <div class="klds-menu__item-icon"
-            style="-webkit-mask-image:url(/assets/icons/ic-trash-empty.svg);mask-image:url(/assets/icons/ic-trash-empty.svg);background-color:var(--klds-color-text-black,#121212);"></div>
-          <span class="klds-menu__item-text">휴지통</span>
-        </div>
+    <div class="klds-menu">
+      <div class="klds-menu__item">
+        <div class="klds-menu__item-icon"
+          style="-webkit-mask-image:url(/assets/icons/ic-mail.svg);mask-image:url(/assets/icons/ic-mail.svg);background-color:var(--klds-color-text-black,#121212);"></div>
+        <span class="klds-menu__item-text">메일 편지함</span>
+      </div>
+      <div class="klds-menu__item is-hover">
+        <div class="klds-menu__item-icon"
+          style="-webkit-mask-image:url(/assets/icons/ic-folder.svg);mask-image:url(/assets/icons/ic-folder.svg);background-color:var(--klds-color-primary,#005386);"></div>
+        <span class="klds-menu__item-text">공용 폴더 (Hover)</span>
+      </div>
+      <div class="klds-menu__item">
+        <div class="klds-menu__item-icon"
+          style="-webkit-mask-image:url(/assets/icons/ic-trash-empty.svg);mask-image:url(/assets/icons/ic-trash-empty.svg);background-color:var(--klds-color-text-black,#121212);"></div>
+        <span class="klds-menu__item-text">휴지통</span>
       </div>
     </div>
   `,
@@ -57,36 +53,34 @@ export const WithIcon = {
 
 export const WithCheckbox = {
   render: () => `
-    <div style="padding:16px;width:240px;">
-      <div class="klds-menu">
-        <label class="klds-menu__item">
-          <div class="klds-menu__item-checkbox">
-            <div class="klds-checkbox">
-              <input type="checkbox" class="klds-checkbox__input">
-              <span class="klds-checkbox__box">${CHECK_SVG}</span>
-            </div>
+    <div class="klds-menu">
+      <label class="klds-menu__item">
+        <div class="klds-menu__item-checkbox">
+          <div class="klds-checkbox">
+            <input type="checkbox" class="klds-checkbox__input">
+            <span class="klds-checkbox__box">${CHECK_SVG}</span>
           </div>
-          <span class="klds-menu__item-text">항목 1</span>
-        </label>
-        <label class="klds-menu__item">
-          <div class="klds-menu__item-checkbox">
-            <div class="klds-checkbox">
-              <input type="checkbox" class="klds-checkbox__input" checked>
-              <span class="klds-checkbox__box">${CHECK_SVG}</span>
-            </div>
+        </div>
+        <span class="klds-menu__item-text">항목 1</span>
+      </label>
+      <label class="klds-menu__item">
+        <div class="klds-menu__item-checkbox">
+          <div class="klds-checkbox">
+            <input type="checkbox" class="klds-checkbox__input" checked>
+            <span class="klds-checkbox__box">${CHECK_SVG}</span>
           </div>
-          <span class="klds-menu__item-text">항목 2 (선택됨)</span>
-        </label>
-        <label class="klds-menu__item">
-          <div class="klds-menu__item-checkbox">
-            <div class="klds-checkbox">
-              <input type="checkbox" class="klds-checkbox__input">
-              <span class="klds-checkbox__box">${CHECK_SVG}</span>
-            </div>
+        </div>
+        <span class="klds-menu__item-text">항목 2 (선택됨)</span>
+      </label>
+      <label class="klds-menu__item">
+        <div class="klds-menu__item-checkbox">
+          <div class="klds-checkbox">
+            <input type="checkbox" class="klds-checkbox__input">
+            <span class="klds-checkbox__box">${CHECK_SVG}</span>
           </div>
-          <span class="klds-menu__item-text">항목 3</span>
-        </label>
-      </div>
+        </div>
+        <span class="klds-menu__item-text">항목 3</span>
+      </label>
     </div>
   `,
 };
